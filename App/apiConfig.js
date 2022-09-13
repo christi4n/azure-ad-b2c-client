7f977d5fcbe9febd61577536888391b4c0874cb8
christi4n <christian@mydevfactory.net>
2022-09-13 06:33:16 +0200

    [TASK] Initial commit

---
+++ App/apiConfig.js
@@ -0,0 +1,8 @@
+// The current application coordinates were pre-registered in a B2C tenant.
+const apiConfig = {
+    // This is taken from the "Expose an API part"
+    // b2cScopes: ["https://mydevfactorynet.onmicrosoft.com/016a8818-682f-4ddc-b503-a85bc5f3df7b/demo.read"],
+    // webApi: "https://mydevfactorynet.onmicrosoft.com/016a8818-682f-4ddc-b503-a85bc5f3df7b"
+    b2cScopes: ["api://016a8818-682f-4ddc-b503-a85bc5f3df7b/demo.read"],
+    webApi: "http://localhost:5000"
+  };
